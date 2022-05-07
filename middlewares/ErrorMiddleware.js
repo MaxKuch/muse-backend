27c11274a5d6cd696a28c5a369993935b93aa427
MaxKuch <narrm4517@gmail.com>
2022-05-07 15:03:11 +0300

    f

--- middlewares/ErrorMiddleware.js
+++ middlewares/ErrorMiddleware.js
@@ -5,5 +5,5 @@ module.exports = function(err, _, res, __) {
     if(err instanceof ApiError) {
         return res.status(err.status).json({ message: err.message, errors: err.errors})
     }
-    return res.status(500).json({message: 'Непредвиденная ошибка', errors: [err]})
+    return res.status(500).json({message: 'Непредвиденная ошибка', errors: [JSON.stringify(err)]})
 }
