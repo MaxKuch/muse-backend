7d90a957af4a7a29351579c63e49e251d67ab30d
MaxKuch <narrm4517@gmail.com>
2022-05-07 16:11:48 +0300

    f

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,8 +6,7 @@ class UserController {
         try {
             const {email, password} = req.body
             const userData = await userService.login(email, password)
-            res.cookie('refreshToken', email, { maxAge: 60 * 24 * 60 * 60, httpOnly: true})
-            res.end()
+            res.cookie('refreshToken', userData.refreshToken, { maxAge: 60 * 24 * 60 * 60, httpOnly: true})
             return res.json(userData)
         } catch (error) {
             next(error)
