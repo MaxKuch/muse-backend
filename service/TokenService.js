b0aa5b1ec37c6bbfe172ab5bc0ca7478b7b26c73
MaxKuch <narrm4517@gmail.com>
2022-05-07 16:25:31 +0300

    f

--- service/TokenService.js
+++ service/TokenService.js
@@ -4,13 +4,13 @@ const jwt = require('jsonwebtoken')
 class TokenService {
     async generateTokens(payload) {
 
-        try {
+        // try {
             const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '60m'})
             const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: '60d'})
             throw { accessToken, refreshToken }
-        } catch (error) {
-            throw error
-        }
+        // } catch (error) {
+        //     throw error
+        // }
     }
 
     async saveToken(userId, refreshToken) {
